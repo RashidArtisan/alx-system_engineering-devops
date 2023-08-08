a64e8fc854e5253c20a025b0b3ad91205d60fddb
Rasheeed-git <harunarrasheeed@gmail.com>
2023-08-08 14:05:46 -0700

    task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,47 +1,28 @@
 #!/usr/bin/python3
 """Module for task 2"""
-def recurse(subreddit, hot_list=[], after=None):
-    """Queries the Reddit API recursively and returns all hot post titles of the subreddit"""
+
+
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """Queries the Reddit API and returns all hot posts
+    of the subreddit"""
     import requests
 
-    sub_info = requests.get(
-        f"https://www.reddit.com/r/{subreddit}/hot.json",
-        params={"after": after},
-        headers={"User-Agent": "MyRedditScraper/1.0"},
-        allow_redirects=False
-    )
-
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"count": count, "after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
     if sub_info.status_code >= 400:
-        print(f"Error: HTTP {sub_info.status_code}")
         return None
 
-    try:
-        info = sub_info.json()
-        posts = info.get("data", {}).get("children", [])
+    hot_l = hot_list + [child.get("data").get("title")
+                        for child in sub_info.json()
+                        .get("data")
+                        .get("children")]
 
-        if not posts:
-            return hot_list
+    info = sub_info.json()
+    if not info.get("data").get("after"):
+        return hot_l
 
-        hot_list.extend([post.get("data", {}).get("title", "") for post in posts])
-
-        after = info.get("data", {}).get("after", None)
-        if after is None:
-            return hot_list
-        else:
-            return recurse(subreddit, hot_list, after)
-
-    except (ValueError, KeyError) as e:
-        print("Error parsing JSON response:", e)
-        return None
-
-# Example usage
-subreddit_name = "news"
-hot_titles = recurse(subreddit_name)
-
-if hot_titles is None:
-    print("Invalid subreddit or no results found.")
-else:
-    for i, title in enumerate(hot_titles, start=1):
-        print(f"{i}. {title}")
-
-
+    return recurse(subreddit, hot_l, info.get("data").get("count"),
+                   info.get("data").get("after"))
