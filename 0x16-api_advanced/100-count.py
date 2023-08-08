09cb66f6f4279d21d40fed34e0de0d76114a6986
Rasheeed-git <harunarrasheeed@gmail.com>
2023-08-08 15:20:10 -0700

    task

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,13 +1,45 @@
-#!/usr/bin/python3
-"""
-100-main
-"""
-import sys
+def count_words(subreddit, word_list, after=None, word_count={}):
+    """Queries the Reddit API recursively, counts occurrences of keywords in hot post titles, and prints the count."""
+    import requests
 
-if __name__ == '__main__':
-    count_words = __import__('100-count').count_words
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
-    else:
-        result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
+    sub_info = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/hot.json",
+        params={"after": after},
+        headers={"User-Agent": "MyRedditScraper/1.0"},
+        allow_redirects=False
+    )
+
+    if sub_info.status_code >= 400:
+        print(f"Error: HTTP {sub_info.status_code}")
+        return
+
+    try:
+        info = sub_info.json()
+        posts = info.get("data", {}).get("children", [])
+
+        for post in posts:
+            title = post.get("data", {}).get("title", "").lower()
+
+            for word in word_list:
+                word = word.lower()
+                if word in title:
+                    word_count[word] = word_count.get(word, 0) + 1
+
+        after = info.get("data", {}).get("after", None)
+        if after is None:
+            sorted_counts = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+            for word, count in sorted_counts:
+                print(f"{word}: {count}")
+            return
+
+        return count_words(subreddit, word_list, after, word_count)
+
+    except (ValueError, KeyError) as e:
+        print("Error parsing JSON response:", e)
+        return
+
+# Example usage
+subreddit_name = "news"
+keywords = ["python", "java", "javascript"]
+count_words(subreddit_name, keywords)
+
